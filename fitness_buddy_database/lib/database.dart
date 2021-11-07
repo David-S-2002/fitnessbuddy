1e04e14577479f332f8e3677fd32c40eb95c9b40
PrestonTL <tpreston@seattleu.edu>
2021-11-06 20:50:45 -0700

    Update database.dart

--- fitness_buddy_database/lib/database.dart
+++ fitness_buddy_database/lib/database.dart
@@ -5,16 +5,18 @@ part of 'fitness_buddy_database.dart';
 
 // Lines 13-25 are copied
 
+// Create a provate constructor
 class DBProvider {
   DBProvider._();
   static final DBProvider db = DBProvider._();
+
   Database _database;
 
+  // Set-up the database
   Future<Database> get database async {
-    if (_database != null) {
-      return _database;
-    }
+    if (_database != null) return _database;
 
+    // If database is null, instantiate it
     _database = await initDB();
     return _database;
   }
@@ -25,55 +27,84 @@ class DBProvider {
     Directory documentsDir = await getApplicationDocumentsDirectory();
 
     // path of database: it's inside that directory
-    String path = join(documentsDir.path, 'fitnessbuddy.db');
+    String path = join(documentsDir.path, "fitnessbuddy.db");
 
-    return await openDatabase(path, version: 1, onOpen: (Database db) async {},
+    return await openDatabase(path, version: 1, onOpen: (db) async {},
         onCreate: (Database db, int version) async {
-      // Create the tables as specified in our ERD using SQLite,
-      // you guys know this part. Creates the table when creating the
-      // db.
-      await db.execute(
-          'CREATE TABLE Exercise (exerciseName STRING, exerciseID' +
-              ' INTEGER PRIMARY KEY, muscleGroup STRING, secondaryMuscleGroup' +
-              ' STRING, equipment STRING, difficulty INTEGER)');
+      // Create the tables as specified in our ERD using SQLite.
+      // Creates the tables when creating the db.
 
-      ('CREATE TABLE MuscleGroup (mucleGroupID INTEGER PRIMARY KEY,' +
-          ' muscleGroup_name TEXT)');
+      await db.execute("CREATE TABLE Exercise ("
+          "exerciseID INTEGER PRIMARY KEY,"
+          "exerciseName TEXT,"
+          "muscleGroup TEXT,"
+          "secondaryMuscleGroup TEXT,"
+          "equipment TEXT,"
+          "difficulty INTEGER"
+          ")");
 
-      ('CREATE TABLE equipment (eq_id INTEGER PRIMARY KEY, eq_name TEXT)');
+      await db.execute("CREATE TABLE MuscleGroup ("
+          "mucleGroupID INTEGER PRIMARY KEY,"
+          "muscleGroup_name TEXT"
+          ")");
 
-      ('CREATE TABLE title (t_id INTEGER PRIMARY KEY, t_name TEXT)');
+      await db.execute("CREATE TABLE Equipment ("
+          "equipmentID INTEGER PRIMARY KEY,"
+          "eq_name TEXT"
+          ")");
+
+      await db.execute("CREATE TABLE Title ("
+          "titleID INTEGER PRIMARY KEY,"
+          "titleNameTEXT"
+          ")");
     });
   }
 
-   Future<int> insertExercise(Exercise exercise) async{
-    final db = await database;
-    var result = await db.insert('Exercise', exercise.toJson());
-    return result;
+  // Exercise is the data model in exercise.dart
+  Future<int> insertExercise(Exercise exercise) async {
+    // db.insert('exercise', exercise.toJson())
+    var result = await db.insert("Exercise", exercise.toJson());
   }
+
   Future<List> getAllExercises() async {
-    final db = await database;
-    var result = await db.query('Exercise', columns: ["exerciseID", 
-    " exerciseName", "muscleGroup", "secondaryMuscleGroup", "equipment",
-    " difficulty"]);
+    var result = await db.query("Exercise", columns: [
+      "exerciseID",
+      " exerciseName",
+      "muscleGroup",
+      "secondaryMuscleGroup",
+      "equipment",
+      " difficulty"
+    ]);
+
     return result.toJson();
   }
-  Future<Exercise> getExercise(int exerciseId) async{
+
+  Future<Exercise> getExercise(int exerciseId) async {
     List<Json> results = await db.query("Exercise",
-    columns: ["exerciseID", "exerciseName", "muscleGroup",
-    "secondaryMuscleGroup", "equipment", "difficulty"],
-    where: 'exerciseID = ?',
-    whereArgs: [exerciseID]);
+        columns: [
+          "exerciseID",
+          "exerciseName",
+          "muscleGroup",
+          "secondaryMuscleGroup",
+          "equipment",
+          "difficulty"
+        ],
+        where: 'exerciseID = ?',
+        whereArgs: [exerciseID]);
+
     if (results.length > 0) {
-      return new Exercise.fromJson(results.first) : null;
+      return new Exercise.fromJson(results.first);
     }
+    return null;
   }
-  Future<int> updateExercise(Exercise exercise) async{
-    return await db.update("Exercise", exercise.toJson(), where: "exerciseID" +
-    " = ?", whereArgs: [exerciseID]);
+
+  Future<int> updateExercise(Exercise exercise) async {
+    return await db.update("Exercise", exercise.toJson(),
+        where: "exerciseID" + " = ?", whereArgs: [exerciseID]);
   }
+
   Future<int> deleteExercise(int exerciseId) async {
-    return await db.delete("Exercise", where: "exerciseID = ?",
-    whereArgs: [exerciseID]);
+    return await db
+        .delete("Exercise", where: "exerciseID = ?", whereArgs: [exerciseID]);
   }
 }
